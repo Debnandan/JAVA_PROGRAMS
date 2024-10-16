e22cc8fed5f898794c30ee5c61b4f0f86f12e92f
DEBNANDAN BERA <116198506+Debnandan@users.noreply.github.com>
2024-10-17 00:25:55 +0530

    Update and rename GCDMyApproach.java to GCD_MyApproach.java
    changing some variable's names

--- PROGRAMS/GCD_MyApproach.java
+++ PROGRAMS/GCD_MyApproach.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class GCDMyApproach {
+public class GCD_MyApproach {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter First Number : ");
