bc0913879792e4bbe6e2585a7b92d301508a21cc
DEBNANDAN BERA <116198506+Debnandan@users.noreply.github.com>
2024-10-20 23:54:46 +0530

    Add files via upload
    Adding Programs

---
+++ PROGRAMS/MethodOverloading.java
@@ -0,0 +1,21 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Sum of two integers: " + calc.add(5, 10));    
+        System.out.println("Sum of three integers: " + calc.add(5, 10, 15)); 
+        System.out.println("Sum of two doubles: " + calc.add(5.5, 3.2));    
+    }
+}
